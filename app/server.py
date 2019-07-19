ff3264ce514b9641c5c9d58c9159671355962dc5
Tom Chmielenski <tchmiel@ptd.net>
2019-07-18 21:27:10 -0400

    update model name in server.py

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://bentley.sharefile.com/d-sed3368e5c564f58b'
+export_file_name = 'teslas-stage-2-v3.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['model_3', 'model_s', 'model_x', 'model_y']
 path = Path(__file__).parent
 
 app = Starlette()
